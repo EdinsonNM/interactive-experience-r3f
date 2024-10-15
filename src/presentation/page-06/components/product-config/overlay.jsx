7bf4802e028b706b24f76dd15cd033cf86e2e937
Edinson Nuñez More <nmedinson@gmail.com>
2024-10-15 13:55:44 -0500

    Update overlay.jsx

--- src/presentation/page-06/components/product-config/overlay.jsx
+++ src/presentation/page-06/components/product-config/overlay.jsx
@@ -70,7 +70,7 @@ export function Overlay() {
                 <button
                   onClick={() => (state.intro = false)}
                   type="button"
-                  className="mt-10 focus:outline-none text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-900"
+                  className="mt-10 focus:outline-none text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-900 pointer-events-auto"
                 >
                   Customizar
                   <AiOutlineHighlight size="1.3em" className="inline-block" />
